721b57631f5940bae5d0185cfad456fa9c703bf5
Marvvo <107701120+Marvvo@users.noreply.github.com>
2026-03-25 11:36:52 +0100

    Update database URI

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # Datenbankkonfiguration aus Umgebungsvariablen
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
-    f"{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
+    f"{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB_Clima')}"
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
